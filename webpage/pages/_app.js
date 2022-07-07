5c58ab08b9163d8ae0d32c00e4c47deaa9a33a57
Federico Torres <80338247+FedericoTorres233@users.noreply.github.com>
2022-07-07 17:50:55 -0300

    feat: añadir pagina web inicial (#29)
    * feat: añadir pagina inicial
    
    * refactor: eliminar codigo de ejemplo
    
    * feat: usar bootstrap
    
    * feat: añadir el tag de Head

---
+++ webpage/pages/_app.js
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Smart Toolbox</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
