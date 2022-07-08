4e22746afb623b9402abd6f3fe3af26ddbf38d52
Rama <ramiromariani5@hotmail.com>
2022-07-08 10:54:20 -0300

    refactor: agrupar rutas en sus carpetas

--- smarttoolboxapp/src/pages/Herramientas/Herramientas.tsx
+++ smarttoolboxapp/src/pages/Herramientas/Herramientas.tsx
@@ -1,6 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Herramientas.css';
+import './Herramientas.module.css';
 
 const Herramientas: React.FC = () => {
   return (
@@ -16,7 +15,6 @@ const Herramientas: React.FC = () => {
             <IonTitle size="large">Herramientas</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Herramientas page" />
       </IonContent>
     </IonPage>
   );
