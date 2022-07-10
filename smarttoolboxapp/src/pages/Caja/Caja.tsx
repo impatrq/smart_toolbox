de8f75b42b6b9b2c4227d6712b28b9faae955aa1
Ramiro Mariani <80327788+RamiroMariani@users.noreply.github.com>
2022-07-10 19:15:57 -0300

    feat: desarrollar pestaña de registro (#36)

--- smarttoolboxapp/src/pages/Caja/Caja.tsx
+++ smarttoolboxapp/src/pages/Caja/Caja.tsx
@@ -1,7 +1,23 @@
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Caja.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { BarcodeScanner } from "@awesome-cordova-plugins/barcode-scanner";
+import "./Caja.css";
 
 const Caja: React.FC = () => {
+  const openScanner = async () => {
+    const data = await BarcodeScanner.scan();
+    console.log(`Barcode data: ${data.text}`);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,6 +26,7 @@ const Caja: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonButton class="B_registro" color="warning" expand="full" shape="round" onClick={openScanner}>Ingrese una Caja</IonButton>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Caja</IonTitle>
