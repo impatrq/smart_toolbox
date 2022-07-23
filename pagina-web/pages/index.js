1c5548cdc81faeebf0b0bb0be8bf3277e04f39e7
Federico Torres <federicofederico881@gmail.com>
2022-07-22 21:51:21 -0300

    feat: añadir @react-spring/parallax al proyecto

--- pagina-web/pages/index.js
+++ pagina-web/pages/index.js
@@ -1,7 +1,10 @@
-import Image from 'next/image'
+import { Parallax } from "@react-spring/parallax";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Pagina inicial</div>
-  )
+    <>
+      <Parallax pages={3} horizontal></Parallax>
+    </>
+  );
 }
