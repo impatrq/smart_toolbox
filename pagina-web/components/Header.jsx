eaabdcd1054caa3081d58aa0d2d0053b0506c499
Federico Torres <federicofederico881@gmail.com>
2022-07-22 22:12:46 -0300

    feat: añadir video de fondo

--- pagina-web/components/Header.jsx
+++ pagina-web/components/Header.jsx
@@ -1,9 +1,17 @@
 import { ParallaxLayer } from "@react-spring/parallax";
+import classes from "../styles/Header.module.css"
 
 export default function Header() {
   return (
     <>
-      <ParallaxLayer speed={1}></ParallaxLayer>
+      <ParallaxLayer speed={1}>
+        <video className={classes.backgroundvideo} autoPlay loop muted>
+          <source
+            src="https://user-images.githubusercontent.com/80338247/180584520-3d8891c8-a111-44a8-bb05-4191fb9ca174.mp4"
+            type="video/mp4"
+          />
+        </video>{" "}
+      </ParallaxLayer>
       <ParallaxLayer speed={2}></ParallaxLayer>
       <ParallaxLayer speed={5}></ParallaxLayer>
     </>
--- pagina-web/pages/index.js
+++ pagina-web/pages/index.js
@@ -1,10 +1,13 @@
 import { Parallax } from "@react-spring/parallax";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
-      <Parallax pages={3} horizontal></Parallax>
+      <Parallax pages={3} horizontal>
+        <Header />
+      </Parallax>
     </>
   );
 }
