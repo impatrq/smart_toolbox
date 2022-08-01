1ad675ae58dbb795798d32ba6887fa3df6cd22ce
unknown <federicofederico881@gmail.com>
2022-08-01 20:30:47 -0300

    fix: arreglar layout del header

--- pagina-web/components/Header.jsx
+++ pagina-web/components/Header.jsx
@@ -17,18 +17,20 @@ export default function Header() {
         speed={2}
         className="text-white text-monospace text-center h-100"
       >
-        <h2 className="pt-5 display-5 fw-bold">Te damos la bienvenida a</h2>
-        <h1 className="display-1 fw-bold">Smart Toolbox</h1>
-        <div className="opacity-75 h-75">
-          <img
-            className="img-fluid h-100"
-            src={"/LogoSinFondo.png"}
-            alt="logo"
-          />
+        <div className={classes.main}>
+          <h2 className="pt-5 display-5 fw-bold">Te damos la bienvenida a</h2>
+          <h1 className="display-1 fw-bold">Smart Toolbox</h1>
+          <div className="opacity-75 h-75">
+            <img
+              className="img-fluid h-100"
+              src={"/LogoSinFondo.png"}
+              alt="logo"
+            />
+          </div>
         </div>
       </ParallaxLayer>
       <ParallaxLayer speed={5}>
-        <Navigate />
+        <Navigate className={classes.navigate} />
       </ParallaxLayer>
     </>
   );
