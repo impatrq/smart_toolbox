ec1d54025eda9d80df0c890e8326c76f498e6ff4
unknown <federicofederico881@gmail.com>
2022-08-03 11:07:13 -0300

    style: añadir componente a la pagina principal

--- pagina-web/pages/index.js
+++ pagina-web/pages/index.js
@@ -1,12 +1,13 @@
 import { Parallax } from "@react-spring/parallax";
-import Image from "next/image";
 import Header from "../components/Header";
+import MainPageSections from "../components/MainPageSections";
 
 export default function Home() {
   return (
     <>
       <Parallax pages={3} horizontal>
         <Header />
+        <MainPageSections />
       </Parallax>
     </>
   );
