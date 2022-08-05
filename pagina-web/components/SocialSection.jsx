4128b4fe8f8d315d8cd8579e94d12916b6329271
Federico Torres <federicofederico881@gmail.com>
2022-08-05 20:52:31 -0300

    feat: añadir titulo de la seccion

--- pagina-web/components/SocialSection.jsx
+++ pagina-web/components/SocialSection.jsx
@@ -1,5 +1,18 @@
+import { ParallaxLayer } from "@react-spring/parallax";
 export default function SocialSection() {
+import { Row, Col } from "react-bootstrap";
+
+export default function SocialMainPage() {
   return (
+    <>
+      <ParallaxLayer offset={2} speed={1.5} className={classes.redes}>
+        <Row>
+          <h1 className="display-1 text-white fw-normal">Redes Sociales</h1>
+        </Row>
+        <Row className={classes.iconCol}>
     <div>SocialSection</div>
   )
+      </ParallaxLayer>
+    </>
+  );
 }
