d368ca9c02966df3e905b940882a31faccf8bb7d
Ramiro Mariani <80327788+RamiroMariani@users.noreply.github.com>
2022-08-10 15:49:40 -0300

    feat: leer tareas de la base de datos (#44)
    * feat: leer tareas de la base de datos
    
    * feat: hacer que se muestren las tareas correctamente

--- aplicacion-movil/src/pages/Tareas/Tareas.tsx
+++ aplicacion-movil/src/pages/Tareas/Tareas.tsx
@@ -11,26 +11,38 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { ref, onValue } from "firebase/database";
+import db from "../../firebase.js";
 
-const checkboxList = [
-  {val: 'Tarea 1', isChecked: false},
-  {val: 'Tarea 2', isChecked: false},
-  {val: 'Tarea 3', isChecked: false},
-];
+const checkboxList: Array<{ val: String; isChecked: boolean }> = [];
 
 const Tareas: React.FC = () => {
+  const [list, setList] = useState(checkboxList);
+  const updateCheckbox = (i: number) => {
+    const lista = JSON.parse(JSON.stringify(list));
+    const item = lista[i];
+    item.isChecked = !item.isChecked;
+    console.log(lista);
+    setList(lista);
+  };
 
-  
-  const [list, setList] = useState(checkboxList)
-  const updateCheckbox = (i:number) => {
-    const lista = JSON.parse(JSON.stringify(list))
-    const item = lista[i]
-    item.isChecked = !item.isChecked
-    console.log(lista)
-    setList(lista)
-  }
-  
+  useEffect(() => {
+    const persona = "p1";
+
+    const starCountRef = ref(db, "sector1/personas/" + persona + "/tareas");
+    onValue(starCountRef, (snapshot) => {
+      const raw_data = snapshot.val();
+      const data = raw_data
+        .split(/\|/gi)
+        .map((i: String) => i.replace(/^\s+|\s+$/gi, ""))
+        .map((i: String) => {
+          return { val: i, isChecked: false };
+        });
+      setList(data);
+    });
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -40,12 +52,15 @@ const Tareas: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonList>
-            {list.map((item, i) => (
-            <IonItem color={item.isChecked ? 'success' : 'danger'} key={i}>
+          {list.map((item, i) => (
+            <IonItem color={item.isChecked ? "success" : "danger"} key={i}>
               <IonLabel>{item.val}</IonLabel>
-              <IonCheckbox value={item.val} onIonChange={() => updateCheckbox(i)}></IonCheckbox>
+              <IonCheckbox
+                value={item.val}
+                onIonChange={() => updateCheckbox(i)}
+              ></IonCheckbox>
             </IonItem>
-            ))}
+          ))}
         </IonList>
       </IonContent>
     </IonPage>
