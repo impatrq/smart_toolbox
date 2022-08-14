7f943b7f39a47a4527a95aa79088b5b981df6773
Federico Torres <federicofederico881@gmail.com>
2022-08-14 14:57:36 -0300

    feat: desarrollar seccion de quienes somos

--- pagina-web/pages/nosotros.js
+++ pagina-web/pages/nosotros.js
@@ -1,12 +1,85 @@
+import ContactCard from "../components/ContactCard";
 import NavBar from "../components/NavBar";
+import { Row } from "react-bootstrap";
 
 export default function nosotros() {
   return (
-    <>
+    <div className="bg-[#130b10] min-h-screen">
       <NavBar />
-      <div className="bg-none text-white fs-1">
-        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      <div className="h-1/2 max-w-full">
+        <div className="grid place-items-center my-7 max-w-full">
+          <img
+            className="opacity-75 max-w-full"
+            src={"/logo_impa.webp"}
+            alt="logo"
+          />
+          <h2 className="text-center text-white text-xl mt-3">
+            Somos un grupo de alumnos de E.E.S.T. N°7 &quot;Taller Regional
+            Quilmes&quot; (IMPA)
+          </h2>
+        </div>
       </div>
-    </>
+      <div className="w-full grid place-items-center">
+        <Row className="w-full">
+          <ContactCard
+            person="Joaquín Martínez"
+            text="Planos y Modelos"
+            github="https://github.com/joacozape04"
+            email="mailto:joaquinosmartinez@gmail.com"
+            ig=""
+          />
+          <ContactCard
+            person="Herrera Lucas"
+            text="Estructura y Circuitos"
+            github="https://github.com/lucas-le-herrera"
+            email="mailto:lucassherreraa2003@gmail.com"
+            ig=""
+          />
+        </Row>
+        <Row className="w-full">
+          <ContactCard
+            person="Ramiro Mariani"
+            text="Estructura y Aplicación móvil"
+            github="https://github.com/RamiroMariani"
+            email="mailto:ramiromariani99@gmail.com"
+            ig=""
+          />
+          <ContactCard
+            person="Federico Torres"
+            text="Microcontrolador, Interfaz gráfica y Página web"
+            github="https://github.com/federicotorres233"
+            email="mailto:federicotorres233@protonmail.com"
+            ig=""
+          />
+        </Row>
+        <Row className="w-full">
+          <ContactCard
+            person="Vanore Miqueas"
+            text="Aplicación móvil"
+            github="https://github.com/MiqueasVanore"
+            email="mailto:vnmiqueas@gmail.com"
+            ig=""
+          />
+          <ContactCard
+            person="Esquivel Agustín"
+            text="Circuitos y PCB"
+            github="https://github.com/Agustin-Esquivel04"
+            email="mailto:esquivel.agustin04@gmail.com"
+            ig=""
+          />
+        </Row>
+      </div>
+      <div className="grid place-items-center my-16">
+        <h1 className="text-white my-7">¡Nuestra Escuela!</h1>
+        <iframe
+          className="rounded-xl"
+          src="https://maps.google.com/maps?width=720&amp;height=600&amp;hl=es&amp;q=Av.%20Otamendi,%20B1878%20Quilmes,%20Provincia%20de%20Buenos%20Aires+(IMPA)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          width="600"
+          height="450"
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+    </div>
   );
 }
