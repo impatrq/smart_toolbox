d46479aaf01166a9d157ce8a7bf9e33cd098911e
Ramiro Mariani <80327788+RamiroMariani@users.noreply.github.com>
2022-08-16 14:02:04 -0300

    feat: crear-lista-en-pestaña-herramientas (#46)

--- aplicacion-movil/src/pages/Herramientas/Herramientas.tsx
+++ aplicacion-movil/src/pages/Herramientas/Herramientas.tsx
@@ -1,7 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Herramientas.module.css';
+import {
+  IonAlert,
+  IonCheckbox,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonItemDivider,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useState, useEffect } from "react";
+import { ref, onValue } from "firebase/database";
+import db from "../../firebase.js";
+
+const checkboxList: Array<{ val: String}> = [];
 
 const Herramientas: React.FC = () => {
+  const [list, setList] = useState(checkboxList);
+
+  useEffect(() => {
+    const caja = "12537865";
+
+    const starCountRef = ref(db, "sector1/cajas/" + caja + "/missing_tools");
+    onValue(starCountRef, (snapshot) => {
+      const raw_data = snapshot.val();
+      const data = raw_data
+        .split(/\|/gi)
+        .map((i: String) => i.replace(/^\s+|\s+$/gi, ""))
+        .map((i: String) => {
+          return { val: i, isChecked: false };
+        });
+      setList(data);
+    });
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,11 +44,13 @@ const Herramientas: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Herramientas</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonList>
+          {list.map((item, i) => (
+            <IonItem color="secondary" key={i}>
+              <IonLabel>{item.val}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
