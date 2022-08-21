fc68b209ab9234e10fe0afc28894fb11377ab308
Federico Torres <federicofederico881@gmail.com>
2022-08-21 11:33:49 -0300

    feat: crear funcion que lea el directorio pictures

--- pagina-web/pages/imagenes.js
+++ pagina-web/pages/imagenes.js
@@ -1,5 +1,21 @@
 import NavBar from "../components/NavBar";
+import fs from "fs";
+import path from "path";
 
-export default function imagenes() {
-  return <NavBar />;
+export default function imagenes({ pictures }) {
+  console.log(pictures);
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("public/pictures"));
+  return {
+    props: {
+      pictures: files,
+    },
+  };
 }
