1d12e4146a451ce01d6d80ba7de4e7e7c5881dff
Federico Torres <federicofederico881@gmail.com>
2022-08-21 14:45:27 -0300

    feat: agregar titulo de la pagina

--- pagina-web/pages/imagenes.js
+++ pagina-web/pages/imagenes.js
@@ -8,6 +8,13 @@ export default function imagenes({ pictures }) {
   return (
     <div className="bg-[#130b10] min-h-screen">
       <NavBar />
+      <div className="h-1/2 max-w-full">
+        <div className="grid place-items-center my-7 max-w-full">
+          <h1 className="text-center text-white text-5xl mt-3">
+            Galería de imágenes
+          </h1>
+        </div>
+      </div>
       <div className="m-10">
         <Galeria images={images} />
       </div>
