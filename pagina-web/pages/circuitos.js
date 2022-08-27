177f5224830e6dd4335449f644f8854e73d4ff5e
Federico Torres <federicofederico881@gmail.com>
2022-08-27 16:28:42 -0300

    feat: agregar titulo

--- pagina-web/pages/circuitos.js
+++ pagina-web/pages/circuitos.js
@@ -1,7 +1,15 @@
-import NavBar from '../components/NavBar'
+import NavBar from "../components/NavBar";
 
 export default function nosotros() {
   return (
-    <NavBar/>
-  )
+    <div className="bg-[#130b10] min-h-screen">
+      <NavBar />
+      <div className="h-1/2 max-w-full">
+        <div className="grid place-items-center my-7 max-w-full">
+          <h1 className="text-center text-white text-5xl mt-3">Circuitos</h1>
+        </div>
+      </div>
+      
+    </div>
+  );
 }
