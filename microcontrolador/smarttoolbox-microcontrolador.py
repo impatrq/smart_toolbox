7505a88954d42c7eb842c7c39563df1a896cdbd4
Federico Torres <80338247+FedericoTorres233@users.noreply.github.com>
2022-08-27 21:47:38 -0300

    feat: continuar codigo microcontrolador (#52)
    * feat: añadir I/O de los contactos magneticos
    
    * feat: empezar el codigo que verifica los contactos
    
    * chore: cambiar pines
    
    * chore: modificar constructor
    
    * feat: añadir indentificacion de herramientas
    
    * feat: añadir seleccion de contacto magnetico
    
    * chore: usar un contacto por el momento
    
    * feat: encender la linea de cada herramienta
    
    * feat: agregar puertos del multiplexor
    
    * feat: comprobar si una herramienta está
    
    * chore: agregar numero de caja
    
    * chore: agregar el atributo de nombre
    
    * feat: hacer un patch al detectar cambios
    
    * fix: cambiar el estado a false
    
    * feat: agregar delays

--- microcontrolador/smarttoolbox-microcontrolador.py
+++ microcontrolador/smarttoolbox-microcontrolador.py
@@ -7,7 +7,7 @@
 import urequests as ureq
 from machine import Pin
 import network
-import time
+from time import sleep_ms, sleep
 
 # Headers
 HTTP_HEADERS = {'Content-Type': 'application/json'}
@@ -24,38 +24,61 @@ dbId = ""
 # DB Url
 dbURL = f"https://smart-toolbox-{dbId}-default-rtdb.firebaseio.com"
 
+# Toolbox Number
+toolbox = "12537865"
+
+# Missing tools
+missing_tools = []
+
+# I/O ports
+contact0 = Pin(25,Pin.IN)
+contact1 = Pin(26,Pin.IN)
+contact2 = Pin(27,Pin.IN)
+
+linea1 = Pin(12, Pin.OUT)
+linea2 = Pin(13, Pin.OUT)
+linea3 = Pin(14, Pin.OUT)
+
+# Multiplexer
+signal = Pin(16,Pin.IN)
+s0 = Pin(0,Pin.OUT)
+s1 = Pin(15,Pin.OUT)
+s2 = Pin(2,Pin.OUT)
+s3 = Pin(4,Pin.OUT)
 
 # Tools constructor
 class Herramientas():
-    def __init__(self,pin,estado):
-        self.pin = pin
-        self.estado = estado
+    def __init__(self,num,sel,nombre,linea):
+        self.num = num
+        self.sel = sel
+        self.nombre = nombre
+        self.linea = linea
 
 # Tools objects
-pinza_fuerza = Herramientas(0,False)
-pinza_punta_1 = Herramientas(1,False)
-pinza_punta_2 = Herramientas(2,False)
-pinza_frenar = Herramientas(3,False)
-pinza_loro = Herramientas(4,False)
-alicates = Herramientas(5,False)
-dest_plano_1 = Herramientas(6,False)
-dest_plano_2 = Herramientas(7,False)
-dest_phillips_1 = Herramientas(8,False)
-dest_phillips_2 = Herramientas(9,False)
-cricket = Herramientas(10,False)
+pinza_fuerza = Herramientas(0,[0,0,0,0],"Pinza de fuerza",1)
+pinza_punta_1 = Herramientas(1,[0,0,0,0],"Pinza de punta 1",1)
+pinza_punta_2 = Herramientas(2,[0,0,0,0],"Pinza de punta 2",1)
+pinza_frenar = Herramientas(3,[0,0,0,0],"Pinza de frenar",1)
+pinza_loro = Herramientas(4,[0,0,0,0],"Pinza pico de loro",1)
+alicates = Herramientas(5,[0,0,0,0],"Alicates",1)
+dest_plano_1 = Herramientas(6,[0,0,0,0],"Destornillador plano 2",1)
+dest_plano_2 = Herramientas(7,[0,0,0,0],"Destornillador plano 2",1)
+dest_phillips_1 = Herramientas(8,[0,0,0,0],"Destornillador Phillips 1",1)
+dest_phillips_2 = Herramientas(9,[0,0,0,0],"Destornillador Phillips 2",1)
+cricket = Herramientas(10,[0,0,0,0],"Cricket",1)
 # ! Llaves combinadas
 # ! Tubos de encastre de 1/4
 # ! Tubos de encastre de 1/4
 # ! Tubos de encastre de 3/8
 # ! Puntas intercambiables
 # ! Extensiones llaves cricket
-multimetro = Herramientas(11,False)
-remachadora = Herramientas(12,False)
-minitorno = Herramientas(13,False)
-linterna = Herramientas(14,False)
-lupa = Herramientas(15,False)
+#multimetro = Herramientas([0,0,0,0],False)
+#remachadora = Herramientas([0,0,0,0],False)
+#minitorno = Herramientas([0,0,0,0],False)
+#linterna = Herramientas([0,0,0,0],False)
+#lupa = Herramientas([0,0,0,0],False)
 
-# * cajon1 = [pinza_fuerza,pinza_punta_1,pinza_punta_2,pinza_frenar,pinza_loro,alicates]
+cajon1 = [pinza_fuerza,pinza_punta_1,pinza_punta_2,pinza_frenar,pinza_loro,alicates]
 
 # ! No se pueden asignar los pines hasta que
 # ! todas las herramientas esten organizadas
@@ -69,18 +92,86 @@ def conectarWifi():
         print('connecting to network...')
         wlan.connect(wifi_essid, wifi_pass)
         while not wlan.isconnected():
-            pass
+            sleep_ms(100)
     print('network config:', wlan.ifconfig())
 
 
 def getReq(param):
     return ureq.get(f"{dbURL}/{area_trabajo}/{param}.json", headers=HTTP_HEADERS).json()
 
-conectarWifi() # Connects to the Wifi network
+def patchReq(param, js):
+    return ureq.patch(f"{dbURL}/{area_trabajo}/{param}.json", json=js ,headers=HTTP_HEADERS).json()
 
-while not getReq("guardar"):
-    time.sleep(5) # Waits until the "guardar" signal arrives
+conectarWifi() # Connects to the Wifi network
 
 #ureq.patch(dbURL,json={"1":"2"}, headers=HTTP_HEADERS)
 
 print("guardar ha cambiado a true")
+
+while True:
+    while not getReq("guardar"):
+        sleep(10) # Waits until the "guardar" signal arrives
+
+    if contact0.value() == 1:
+        for tool in cajon1:
+            if tool.linea == 1:
+                linea1.on()
+            elif tool.linea == 2:
+                linea2.on()
+            elif tool.linea == 3:
+                linea3.on()
+
+            sleep_ms(50)
+
+            if tool.sel[0] == 1:
+                s0.on()
+            if tool.sel[1] == 1:
+                s1.on()
+            if tool.sel[2] == 1:
+                s2.on()
+            if tool.sel[3] == 1:
+                s3.on()
+
+            sleep_ms(50)
+
+            if signal == 1:
+                missing_tools.append(tool.nombre)
+                patchReq(f"cajas/{toolbox}",{"missing_tools": " | ".join(missing_tools), "state": True})
+            elif signal == 0:
+                missing_tools.remove(tool.nombre)
+                patchReq(f"cajas/{toolbox}",{"missing_tools": " | ".join(missing_tools), "state": False})
+
+            s0.off()
+            s1.off()
+            s2.off()
+            s3.off()
+
+            sleep_ms(500)
+
+
+    # contact = 0
+    # while not contact > 2:
+
+    #     if contact == 0:
+    #         if contact0.value():
+    #             selector = 0
+    #             while selector < 15:
+    #                 selector += 1
+    #         else:
+    #             contact += 1
+
+    #     elif contact1 == 1:
+    #         if contact1.value():
+    #             selector = 0
+    #             while selector < 15:
+    #                 selector += 1
+    #         else:
+    #             contact += 1
+
+    #     elif contact2 == 2:
+    #         if contact2.value():
+    #             selector = 0
+    #             while selector < 15:
+    #                 selector += 1
+    #         else:
+    #             contact += 1
