c43c0f1f249fb546df2026835ca1e0cd7ff787be
unknown <federicofederico881@gmail.com>
2022-09-14 22:50:03 -0300

    feat: crear pagina de inicio

--- interfaz-grafica/src/App.tsx
+++ interfaz-grafica/src/App.tsx
@@ -4,11 +4,13 @@ import db from "./firebase";
 
 function App() {
   return (
-    <>
+    <main style={{ maxWidth: "100vw", minHeight: "100vh" }}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/tareas" element={<Home />} />
+        <Route path="/cajas" element={<Home />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
--- interfaz-grafica/src/Home.tsx
+++ interfaz-grafica/src/Home.tsx
@@ -1,9 +1,68 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
+import db from "./firebase";
 
 export default function Home() {
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+  console.log(isSwitchOn);
+  const divStyle = {
+    maxWidth: "100vw",
+    minHeight: "100vh",
+    display: "grid",
+    "place-items": "center",
+  };
+  const itemStyle = { display: "grid", "place-items": "center" };
+  const link = {
+    display: "grid",
+    "place-items": "center",
+    width: "100%",
+    color: "black",
+    textDecoration: "none",
+  };
   return (
-    <div>Home</div>
-  )
+    <div style={divStyle}>
+      <h2>
+        <strong>Interfaz gráfica de Smart Toolbox</strong>
+      </h2>
+      <Container>
+        <Row>
+          <Col style={itemStyle}>
+            <Link style={link} to="/tareas">
+              <Button style={{ width: "50%" }}>
+                <strong>Tareas</strong>
+              </Button>
+            </Link>
+          </Col>
+          <Col style={itemStyle}>
+            <Link style={link} to="/cajas">
+              <Button style={{ width: "50%" }}>
+                <strong>Cajas</strong>
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+        <div
+          style={{ marginTop: "3rem", display: "grid", placeItems: "center" }}
+        >
+          <Row>
+            <Form>
+              <Form.Check
+                onChange={() => setIsSwitchOn(!isSwitchOn)}
+                checked={isSwitchOn}
+                style={{ fontSize: "2rem" }}
+                type="switch"
+                id="custom-switch"
+                label=""
+              />
+            </Form>
+          </Row>
+        </div>
+      </Container>
+    </div>
+  );
 }
