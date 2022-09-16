7f8004a51d9893c5c66b98bc1ef8e0f986af9a64
Federico Torres <80338247+FedericoTorres233@users.noreply.github.com>
2022-09-16 00:01:48 -0300

    feat: implementar señal guardado interfaz grafica (#61)
    * refactor: borrar dependencias sin usar
    
    * refactor: cambiar favicon
    
    * feat: añadir bootstrap
    
    * feat: añadir firebase al proyecto
    
    * feat: crear rutas de la app
    
    * feat: crear pagina de inicio
    
    * feat: enviar señal a la base datos

--- interfaz-grafica/src/App.tsx
+++ interfaz-grafica/src/App.tsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import db from "./firebase";
+
 function App() {
-  return <div>Welcome to Smart Toolbox Interface</div>;
+  return (
+    <main style={{ maxWidth: "100vw", minHeight: "100vh" }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tareas" element={<Home />} />
+        <Route path="/cajas" element={<Home />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
--- interfaz-grafica/src/index.tsx
+++ interfaz-grafica/src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,6 +11,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
