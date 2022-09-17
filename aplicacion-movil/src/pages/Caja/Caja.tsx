e77d732c03f70119773a8052f41c37c4f8711a5d
Federico Torres <80338247+FedericoTorres233@users.noreply.github.com>
2022-09-16 23:43:06 -0300

    feat: enviar datos con update

--- aplicacion-movil/src/pages/Caja/Caja.tsx
+++ aplicacion-movil/src/pages/Caja/Caja.tsx
@@ -8,33 +8,26 @@ import {
 } from "@ionic/react";
 import { BarcodeScanner } from "@awesome-cordova-plugins/barcode-scanner";
 import "./Caja.css";
-import { update } from "firebase/database";
+import { update, ref } from "firebase/database";
 import db from "../../firebase.js";
 import { useState } from "react";
 
 //ionic build
 //npx cap add android
-//export CAPACITOR_ANDROID_STUDIO_PATH=/snap/bin/android-studio 
+//export CAPACITOR_ANDROID_STUDIO_PATH=/snap/bin/android-studio
 //npx cap open android
 
 const Caja: React.FC = () => {
-
-  const [code, setCode] = useState("")
+  const [code, setCode] = useState("");
+  const nombre = "juan";
   const openScanner = async () => {
     const data = await BarcodeScanner.scan();
-    setCode(data.text)
+    setCode(data);
+    const obj = {};
+    const url = `/sector1/personas/${nombre}/caja`;
+    obj[url] = data;
+    update(ref(db), obj);
   };
-
-// const Update = () => {
-    
-
-//       const postData = {
-//         uid: uid,
-//       };
-
-//       const newUpdateKey = update(uid)
-
-//   };
 
   return (
     <IonPage>
@@ -44,8 +37,15 @@ const Caja: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonButton class="B_registro" color="warning" expand="full" shape="round" onClick={openScanner}>Ingrese una Caja</IonButton>
-          <IonTitle>{code}</IonTitle>
+        <IonButton
+          class="B_registro"
+          color="warning"
+          expand="full"
+          shape="round"
+          onClick={openScanner}
+        >
+          Ingrese una Caja
+        </IonButton>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Caja</IonTitle>
