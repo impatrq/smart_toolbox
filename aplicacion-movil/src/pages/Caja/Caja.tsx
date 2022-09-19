626425eb379a9e6bdbe74da2ade6197c122c86cf
Ramiro Mariani <80327788+RamiroMariani@users.noreply.github.com>
2022-09-19 15:11:50 -0300

    feat: enviar qr base de datos (#64)
    * feat: enviar datos con update
    
    * fix: arreglar tipos de typescript
    
    Co-authored-by: Federico Torres <80338247+FedericoTorres233@users.noreply.github.com>

--- aplicacion-movil/src/pages/Caja/Caja.tsx
+++ aplicacion-movil/src/pages/Caja/Caja.tsx
@@ -6,35 +6,30 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { BarcodeScanner } from "@awesome-cordova-plugins/barcode-scanner";
+import { BarcodeScanner, BarcodeScanResult } from "@awesome-cordova-plugins/barcode-scanner";
 import "./Caja.css";
-import { update } from "firebase/database";
+import { update, ref } from "firebase/database";
 import db from "../../firebase.js";
 import { useState } from "react";
 
+interface barCode {
+  [url: string]: number;
+}
+
 //ionic build
 //npx cap add android
-//export CAPACITOR_ANDROID_STUDIO_PATH=/snap/bin/android-studio 
+//export CAPACITOR_ANDROID_STUDIO_PATH=/snap/bin/android-studio
 //npx cap open android
 
 const Caja: React.FC = () => {
-
-  const [code, setCode] = useState("")
+  const nombre = "juan";
   const openScanner = async () => {
     const data = await BarcodeScanner.scan();
-    setCode(data.text)
+    const obj: barCode = {};
+    const url: string = `/sector1/personas/${nombre}/caja`;
+    obj[url] = Number(data.text);
+    update(ref(db), obj);
   };
-
-// const Update = () => {
-    
-
-//       const postData = {
-//         uid: uid,
-//       };
-
-//       const newUpdateKey = update(uid)
-
-//   };
 
   return (
     <IonPage>
@@ -44,8 +39,15 @@ const Caja: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonButton class="B_registro" color="warning" expand="full" shape="round" onClick={openScanner}>Ingrese una Caja</IonButton>
-          <IonTitle>{code}</IonTitle>
+        <IonButton
+          class="B_registro"
+          color="warning"
+          expand="full"
+          shape="round"
+          onClick={openScanner}
+        >
+          Ingrese una Caja
+        </IonButton>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Caja</IonTitle>
