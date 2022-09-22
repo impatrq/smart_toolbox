0df7f969dca00ae1935d100eb09e1cf785a38fa1
unknown <federicofederico881@gmail.com>
2022-09-21 21:43:00 -0300

    chore: cambiar nombre de la app

--- aplicacion-movil/capacitor.config.ts
+++ aplicacion-movil/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'smarttoolboxapp',
+  appName: 'Smart Toolbox',
   webDir: 'build',
   bundledWebRuntime: false
 };
