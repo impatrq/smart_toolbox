6736008247cdd245f8586a53a7fa42f85c905385
unknown <federicofederico881@gmail.com>
2022-09-22 21:58:35 -0300

    feat: agregar tabla

---
+++ interfaz-grafica/src/Caja.tsx
@@ -0,0 +1,37 @@
+import Container from "react-bootstrap/Container";
+import { useState, useEffect } from "react";
+import { ref, onValue } from "firebase/database";
+import Table from "react-bootstrap/Table";
+import db from "./firebase";
+
+export default function Caja() {
+    
+    const divStyle = {
+        maxWidth: "100vw",
+        minHeight: "100vh",
+        display: "grid",
+        "place-items": "center",
+    };
+    const itemStyle = { display: "grid", "place-items": "center" };
+    const link = {
+        display: "grid",
+        "place-items": "center",
+        width: "100%",
+        color: "black",
+        textDecoration: "none",
+    };
+    return (
+        <div style={divStyle}>
+            <div style={{ display: "grid", placeItems: "center" }}>
+                <h2 style={{ marginBottom: "3rem" }}>
+                    Cajas de este sector
+                </h2>
+                <Container>
+                    <Table striped bordered hover variant="dark">
+                        
+                    </Table>
+                </Container>
+            </div>
+        </div>
+    );
+}
