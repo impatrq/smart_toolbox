f7db392cf01375f3a9dcf32790ff565c6ca19948
unknown <federicofederico881@gmail.com>
2022-09-29 20:52:10 -0300

    refactor: cambiar a OOP

--- microcontrolador/primer_prototipo.py
+++ microcontrolador/primer_prototipo.py
@@ -6,202 +6,96 @@
 
 from machine import Pin
 import time
-##Contador Binario de 4 bits
-count=0
 
-#button = machine.Pin(0, machine.Pin.IN)
-bit1 = Pin(17, Pin.OUT)
-bit2 = Pin(16, Pin.OUT)
-bit3 = Pin(4, Pin.OUT)
-bit4 = Pin(2, Pin.OUT)
-sig = Pin(15,Pin.IN)
+##Contador Binario de 4 bits
+count = 0
+
+s0 = Pin(17, Pin.OUT)
+s1 = Pin(16, Pin.OUT)
+s2 = Pin(4, Pin.OUT)
+s3 = Pin(2, Pin.OUT)
+sig = Pin(15, Pin.IN)
 
 missing_tools = []
 
+# Tools constructor
+class Herramientas:
+    def __init__(self, num, sel, nombre):
+        self.num = num
+        self.sel = sel
+        self.nombre = nombre
+
+
+# Tools objects
+martillo = Herramientas(0, [0, 0, 0, 0], "Martillo")
+fuerza = Herramientas(1, [0, 0, 0, 1], "Pinza de fuerza")
+cutter = Herramientas(2, [0, 0, 1, 0], "Cutter")
+allen = Herramientas(3, [0, 0, 1, 1], "Llaves allen")
+pelacables = Herramientas(4, [0, 1, 0, 0], "Pelacables")
+punta1 = Herramientas(5, [0, 1, 0, 1], "Pinza de punta 1")
+alicates = Herramientas(6, [0, 1, 1, 0], "Alicates")
+punta2 = Herramientas(7, [0, 1, 1, 1], "Pinza de punta 2")
+loro = Herramientas(8, [1, 0, 0, 0], "Pico de loro")
+inglesa = Herramientas(9, [1, 0, 0, 1], "Llave inglesa")
+frenar = Herramientas(10, [1, 0, 1, 0], "Pinza de frenar")
+phillips1 = Herramientas(11, [1, 0, 1, 1], "Phillips 1")
+plano1 = Herramientas(12, [1, 1, 0, 0], "Plano 1")
+phillips2 = Herramientas(13, [1, 1, 0, 1], "Phillips 2")
+plano2 = Herramientas(14, [1, 1, 1, 0], "Plano 2")
+metrica = Herramientas(15, [1, 1, 1, 1], "Cinta metrica")
+
+
+def checkDuplicates(tool):
+    return tool in missing_tools
+
+
+tools = [
+    martillo,
+    fuerza,
+    cutter,
+    allen,
+    pelacables,
+    punta1,
+    alicates,
+    punta2,
+    loro,
+    inglesa,
+    frenar,
+    phillips1,
+    plano1,
+    phillips2,
+    plano2,
+    metrica,
+]
+
 while True:
 
-  time.sleep(1)
+    time.sleep(0.5)
 
-  if (True):
-    print(sig.value())
-    count += 1
+    count = 1
 
-    if (count == 1):
-      bit1.off()
-      bit2.off()
-      bit3.off()
-      bit4.off()
-      if(sig.value()):
-          missing_tools.append("Martillo")
-      else:
-          if "Martillo" in missing_tools:
-              missing_tools.remove("Martillo")
+    for tool in tools:
+        if tool.sel[3] == 1:
+            s0.on()
+        if tool.sel[2] == 1:
+            s1.on()
+        if tool.sel[1] == 1:
+            s2.on()
+        if tool.sel[0] == 1:
+            s3.on()
+        time.sleep_ms(50)
+        print(str(s3.value()) + str(s2.value()) + str(s1.value()) + str(s0.value()))
+        if sig.value():
+            if not checkDuplicates(tool.nombre):
+                missing_tools.append(tool.nombre)
+        else:
+            if checkDuplicates(tool.nombre):
+                missing_tools.remove(tool.nombre)
+        s0.off()
+        s1.off()
+        s2.off()
+        s3.off()
+    time.sleep(1)
 
-    elif(count == 2):
-      bit1.on()
-      bit2.off()
-      bit3.off()
-      bit3.off()
-      if(sig.value()):
-          missing_tools.append("Pinza de fuerza")
-      else:
-          if "Pinza de fuerza" in missing_tools:
-              missing_tools.remove("Pinza de fuerza")
-
-    elif(count == 3):
-      bit1.off()
-      bit2.on()
-      bit3.off()
-      bit4.off()
-      if(sig.value()):
-          missing_tools.append("Cutter")
-      else:
-          if "Cutter" in missing_tools:
-              missing_tools.remove("Cutter")
-
-    elif(count == 4):
-      bit1.on()
-      bit2.on()
-      bit3.off()
-      bit4.off()
-      if(sig.value()):
-          missing_tools.append("Llaves allen")
-      else:
-          if "Llaves allen" in missing_tools:
-              missing_tools.remove("Llaves allen")
-
-    elif(count == 5):
-      bit1.off()
-      bit2.off()
-      bit3.on()
-      bit4.off()
-      if(sig.value()):
-          missing_tools.append("Pelacables")
-      else:
-          if "Pelacables" in missing_tools:
-              missing_tools.remove("Pelacables")
-
-    elif(count == 6):
-      bit1.on()
-      bit2.off()
-      bit3.on()
-      bit4.off()
-      if(sig.value()):
-          missing_tools.append("Pinza de punta 1")
-      else:
-          if "Pinza de punta 1" in missing_tools:
-              missing_tools.remove("Pinza de punta 1")
-
-    elif(count == 7):
-      bit1.off()
-      bit2.on()
-      bit3.on()
-      bit4.off()
-      if(sig.value()):
-          missing_tools.append("Alicates")
-      else:
-          if "Alicates" in missing_tools:
-              missing_tools.remove("Alicates")
-
-    elif(count == 8):
-      bit1.on()
-      bit2.on()
-      bit3.on()
-      bit4.off()
-      if(sig.value()):
-          missing_tools.append("Pinza de punta 2")
-      else:
-          if "Pinza de punta 2" in missing_tools:
-              missing_tools.remove("Pinza de punta 2")
-
-    elif(count == 9):
-      bit1.off()
-      bit2.off()
-      bit3.off()
-      bit4.on()
-      if(sig.value()):
-          missing_tools.append("Pico de loro")
-      else:
-          if "Pico de loro" in missing_tools:
-              missing_tools.remove("Pico de loro")
-
-    elif(count == 10):
-      bit1.on()
-      bit2.off()
-      bit3.off()
-      bit4.on()
-      if(sig.value()):
-          missing_tools.append("Llave inglesa")
-      else:
-          if "Llave inglesa" in missing_tools:
-              missing_tools.remove("Llave inglesa")
-
-    elif(count == 11):
-      bit1.off()
-      bit2.on()
-      bit3.off()
-      bit4.on()
-      if(sig.value()):
-          missing_tools.append("Pinza de frenar")
-      else:
-          if "Pinza de frenar" in missing_tools:
-              missing_tools.remove("Pinza de frenar")
-
-    elif(count == 12):
-      bit1.on()
-      bit2.on()
-      bit3.off()
-      bit4.on()
-      if(sig.value()):
-          missing_tools.append("Phillips 1")
-      else:
-          if "Phillips 1" in missing_tools:
-              missing_tools.remove("Phillips 1")
-
-    elif(count == 13):
-      bit1.off()
-      bit2.off()
-      bit3.on()
-      bit4.on()
-      if(sig.value()):
-          missing_tools.append("Plano 1")
-      else:
-          if "Plano 1" in missing_tools:
-              missing_tools.remove("Plano 1")
-
-    elif(count == 14):
-      bit1.on()
-      bit2.off()
-      bit3.on()
-      bit4.on()
-      if(sig.value()):
-          missing_tools.append("Phillips 2")
-      else:
-          if "Phillips 2" in missing_tools:
-              missing_tools.remove("Phillips 2")
-
-    elif(count == 15):
-      bit1.off()
-      bit2.on()
-      bit3.on()
-      bit4.on()
-      if(sig.value()):
-          missing_tools.append("Plano 2")
-      else:
-          if "Plano 2" in missing_tools:
-              missing_tools.remove("Plano 2")
-
-    elif(count == 16):
-      bit1.on()
-      bit2.on()
-      bit3.on()
-      bit4.on()
-      if(sig.value()):
-          missing_tools.append("Cinta")
-      else:
-          if "Cinta" in missing_tools:
-              missing_tools.remove("Cinta")
-      count = 0
     print(missing_tools)
-    print(str(bit4.value()) + str(bit3.value()) + str(bit2.value()) + str(bit1.value()))
 
