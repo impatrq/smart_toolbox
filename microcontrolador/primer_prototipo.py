8a564ecc764d44de941897265556adde8677ca3d
unknown <federicofederico881@gmail.com>
2022-09-29 21:08:25 -0300

    docs: agregar comentarios

--- microcontrolador/primer_prototipo.py
+++ microcontrolador/primer_prototipo.py
@@ -7,46 +7,52 @@
 from machine import Pin
 import time
 
+# I/O Ports
 s0 = Pin(17, Pin.OUT)
 s1 = Pin(16, Pin.OUT)
 s2 = Pin(4, Pin.OUT)
 s3 = Pin(2, Pin.OUT)
 sig = Pin(15, Pin.IN)
-
 alarm = Pin(13, Pin.OUT)
 
 alarm.off()
 missing_tools = []
 
 # Tools constructor
-class Herramientas:
+
+
+class Tools():
     def __init__(self, num, sel, nombre):
         self.num = num
         self.sel = sel
         self.nombre = nombre
 
 
-# Tools objects
-martillo = Herramientas(0, [0, 0, 0, 0], "Martillo")
-fuerza = Herramientas(1, [0, 0, 0, 1], "Pinza de fuerza")
-cutter = Herramientas(2, [0, 0, 1, 0], "Cutter")
-allen = Herramientas(3, [0, 0, 1, 1], "Llaves allen")
-pelacables = Herramientas(4, [0, 1, 0, 0], "Pelacables")
-punta1 = Herramientas(5, [0, 1, 0, 1], "Pinza de punta 1")
-alicates = Herramientas(6, [0, 1, 1, 0], "Alicates")
-punta2 = Herramientas(7, [0, 1, 1, 1], "Pinza de punta 2")
-loro = Herramientas(8, [1, 0, 0, 0], "Pico de loro")
-inglesa = Herramientas(9, [1, 0, 0, 1], "Llave inglesa")
-frenar = Herramientas(10, [1, 0, 1, 0], "Pinza de frenar")
-phillips1 = Herramientas(11, [1, 0, 1, 1], "Phillips 1")
-plano1 = Herramientas(12, [1, 1, 0, 0], "Plano 1")
-phillips2 = Herramientas(13, [1, 1, 0, 1], "Phillips 2")
-plano2 = Herramientas(14, [1, 1, 1, 0], "Plano 2")
-metrica = Herramientas(15, [1, 1, 1, 1], "Cinta metrica")
+# Tool objects
+martillo = Tools(0, [0, 0, 0, 0], "Martillo")
+fuerza = Tools(1, [0, 0, 0, 1], "Pinza de fuerza")
+cutter = Tools(2, [0, 0, 1, 0], "Cutter")
+allen = Tools(3, [0, 0, 1, 1], "Llaves allen")
+pelacables = Tools(4, [0, 1, 0, 0], "Pelacables")
+punta1 = Tools(5, [0, 1, 0, 1], "Pinza de punta 1")
+alicates = Tools(6, [0, 1, 1, 0], "Alicates")
+punta2 = Tools(7, [0, 1, 1, 1], "Pinza de punta 2")
+loro = Tools(8, [1, 0, 0, 0], "Pico de loro")
+inglesa = Tools(9, [1, 0, 0, 1], "Llave inglesa")
+frenar = Tools(10, [1, 0, 1, 0], "Pinza de frenar")
+phillips1 = Tools(11, [1, 0, 1, 1], "Phillips 1")
+plano1 = Tools(12, [1, 1, 0, 0], "Plano 1")
+phillips2 = Tools(13, [1, 1, 0, 1], "Phillips 2")
+plano2 = Tools(14, [1, 1, 1, 0], "Plano 2")
+metrica = Tools(15, [1, 1, 1, 1], "Cinta metrica")
 
+
+# Finds if a tool is in the missing_tools list
 def checkDuplicates(tool):
     return tool in missing_tools
 
+
+# this makes the tools iterable
 tools = [
     martillo,
     fuerza,
@@ -66,6 +72,7 @@ tools = [
     metrica,
 ]
 
+# Main event loop
 while True:
 
     for tool in tools:
@@ -77,13 +84,20 @@ while True:
             s2.on()
         if tool.sel[0] == 1:
             s3.on()
+
+        # ! This delay ensures a effective multiplexer switching
         time.sleep_ms(50)
-        print(str(s3.value()) + str(s2.value()) + str(s1.value()) + str(s0.value()))
+
+        print(str(s3.value()) + str(s2.value()) +
+              str(s1.value()) + str(s0.value()))
+
         if sig.value():
+            # * The tool is not in its place
             if not checkDuplicates(tool.nombre):
                 missing_tools.append(tool.nombre)
                 alarm.on()
         else:
+            # * The tool is in its place
             if checkDuplicates(tool.nombre):
                 alarm.off()
                 missing_tools.remove(tool.nombre)
@@ -91,6 +105,7 @@ while True:
         s1.off()
         s2.off()
         s3.off()
+
     time.sleep(1)
 
     print(missing_tools)
